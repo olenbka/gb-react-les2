c9693890bf63c82a67fe7106a04d30da8821b3b4
Olga <olenbka@inbox.ru>
2021-11-11 17:30:27 +0100

    les6

--- src/homework-4/components/MessageList.js
+++ src/homework-4/components/MessageList.js
@@ -1,9 +1,25 @@
-import { Message } from "./Message"
+import propTypes from "prop-types";
+import { Message } from "./Message";
 
-export const MessageList = ({messageList}) => {
-    return <ul>{
-        messageList.map((message, idx) => <li key={idx}>
-            <Message {...message} />
-        </li>)
-    }</ul>
-}
+export const MessageList = (props) => {
+    return (
+        <div>
+            {props.messageList.map((item) => (
+                <Message key={item.id} {...item} />
+            ))}  
+        </div>      
+    );            
+};        
+
+MessageList.propTypes = {
+    messageList: propTypes.arrayOf(
+        propTypes.shape({
+            id: propTypes.string,
+            text: propTypes.string,
+            author: propTypes.string
+        })
+    )
+};
+MessageList.defaultProps = {
+    messageList: []
+};
--- src/homework-4/routes/Messages/index.js
+++ src/homework-4/routes/Messages/index.js
@@ -1,45 +1,55 @@
 import React from "react";
-import { useState, useEffect, useCallback } from "react";
+import { useEffect, useCallback } from "react";
+import { useDispatch, useSelector} from "react-redux";
 import { useParams, Redirect } from "react-router-dom";
 import { MessageInput } from "../../components/MessageInput";
 import { MessageList } from "../../components/MessageList";
-import { CHATS } from "../../mocks/chats";
+import { getChatMessagesById } from "../../store/messages/selectors";
+import { createMessage } from "../../store/messages/actions";
+import { hasChatById } from "../../store/chats/selectors";
 
 export const Messages = () => {
     const { chatId } = useParams();
-    const [ messageList, setMessageList ] = useState([]);
+    const dispatch = useDispatch();
 
-    const sendMessage = (author, text) => {
-        const newMessageList = [...messageList];
+    console.log('getChatMessagesById(chatId)', getChatMessagesById(chatId), useSelector(getChatMessagesById(chatId)))
+
+    const messageList = useSelector((state) => {
+        console.log('state', state)
+        return getChatMessagesById(chatId)(state)
+    });
+    const hasChat = useSelector(hasChatById(chatId));
+
+    const sendMessage = useCallback((author, text) => {
         const newMessage = {
             author,
             text
-        }
-        newMessageList.push(newMessage);
-
-        setMessageList(newMessageList);
-    };
+        };
+        dispatch(createMessage(newMessage, chatId))
+    }, [dispatch, chatId]);
 
     const onSendMessage = (value) => {
         sendMessage("user", value);
     };
 
     useEffect(() => {
-        if (messageList.length === 0) {
+        if (!messageList || messageList.length === 0) {
             return
         }
 
-        const tail = messageList[messageList.lengt - 1];
-        if (tail.author === "bot") {
-            return;
-        }
+        // const tail = messageList[messageList.lenght - 1];
+        // if (tail.author==="bot") {
+        //     return;
+        // }
 
-        sendMessage("bot", "hello");
-    }, [messageList]);
+        // sendMessage("bot", "hello");
+    }, [sendMessage, messageList]);
 
-    if (!CHATS.find(({ id }) => id === chatId)) {
+    if (!hasChat) {
         return <Redirect to ="/chats" />;
     }
+
+    console.log('messageList', messageList)
 
     return (
         <>
